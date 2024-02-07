a234a452ff3e887ded0132213c28bfc5b1898755
takeru.hashimoto <hstake25@gmail.com>
2024-02-07 21:38:00 +0900

    official code

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,17 @@
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices#solution
 
 import { PrismaClient } from '@prisma/client'
 
-const client = global.prismadb || new PrismaClient()
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-if (process.env.NODE_ENV === 'production') global.prisma = client
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-export default client
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
